5e8cb36566f2b7f1f4191ec9e352523afa08fe71
rutger schoone <rcschoone@gmail.com>
2023-02-15 14:35:52 +0100

    made some comments and created validations

--- app/models/art.rb
+++ app/models/art.rb
@@ -1,4 +1,13 @@
 class Art < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  # validations
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { only_integer: true }, greater_than: 0
+  validates :location, presence: true
+  validates :size, presence: true, numericality: { only_integer: true }, greater_than: 0
+  validates :user, presence: true
+  validates :category, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # resources for Art model
   resources :arts, only: %i[index new create]
 end
