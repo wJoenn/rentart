c298b45484a5bc0732b3419907a2c4c0bd04d138
Louis Ramos <wjoenn@gmail.com>
2023-02-16 08:58:45 +0100

    Updated user model validations and attached pphoto

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :photo
+
+  validates :first_name, :last_name, :birthdate, presence: true
+  validate :older_than_eighteen
+
+  private
+
+  def older_than_eighteen
+    return unless birthdate.present? && 18.years.ago < birthdate
+
+    errors.add(:birthdate, "must be older than 18 years old")
+  end
 end
