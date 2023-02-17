6766812cb88191d9385a0c34e57e1f5f20734276
rutger schoone <rcschoone@gmail.com>
2023-02-17 12:57:06 +0100

    removed category from validations

--- app/models/art.rb
+++ app/models/art.rb
@@ -4,7 +4,7 @@ class Art < ApplicationRecord
   has_many_attached :photos
 
   # validations
-  validates :title, :location, :description, :user, :category, :photos, presence: true
+  validates :title, :location, :description, :user, :photos, presence: true
   validates :price, presence: true, greater_than: 0
   validates :height, :width, numericality: { only_integer: true }, greater_than: 0
 end
