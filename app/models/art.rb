f4825fdf18c4116f49420ddd061305890366bf4a
Louis Ramos <wjoenn@gmail.com>
2023-02-18 17:46:17 +0100

    Fixed issue where width and height could not be nil

--- app/models/art.rb
+++ app/models/art.rb
@@ -2,8 +2,8 @@ class Art < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_many_attached :photos
-  
+
   validates :title, :location, :description, :user, :photos, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
-  validates :height, :width, numericality: { only_integer: true, greater_than: 0 }
+  validates :height, :width, numericality: { only_integer: true, greater_than: 0 }, allow_nil: true
 end
