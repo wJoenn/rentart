31b3f2d177bd68f5dbd04834449a5fd86cf03ba0
rutger schoone <rcschoone@gmail.com>
2023-02-18 15:05:47 +0100

    bigfixes category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_16_075532) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_17_193124) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,27 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_16_075532) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "arts", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.float "price"
+    t.string "location"
+    t.integer "height"
+    t.integer "width"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "category_id", null: false
+    t.index ["category_id"], name: "index_arts_on_category_id"
+    t.index ["user_id"], name: "index_arts_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -60,4 +81,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_16_075532) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "arts", "categories"
+  add_foreign_key "arts", "users"
 end
