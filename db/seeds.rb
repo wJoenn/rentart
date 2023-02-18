a9171048445fa996c62d57b1ef92ffd7238e58bf
Louis Ramos <wjoenn@gmail.com>
2023-02-18 17:46:32 +0100

    Created seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,64 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "open-uri"
+
+puts "=== DESTROYING TABLES ==="
+
+Category.destroy_all
+puts "Categories destroyed"
+
+User.destroy_all
+puts "User destroyed"
+
+puts "\n=== CREATING CATEGORIES ==="
+
+5.times do
+  name = Faker::Book.genre
+  category = Category.new(name: name)
+
+  photo = URI.open("https://source.unsplash.com/random")
+  category.photo.attach(io: photo, filename: "#{name}.png", content_type: "image/png")
+
+  category.save!
+
+  puts "#{category.name} added to the categories"
+end
+
+2.times do
+  puts "\n=== CREATING User ==="
+
+  user_params = {
+    first_name: Faker::Name.first_name,
+    last_name:Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: "password",
+    birthdate: Faker::Date.birthday(min_age: 18)
+  }
+
+  user = User.create!(user_params)
+
+  puts "#{user_params[:first_name]} #{user_params[:last_name]} added to the users"
+
+  puts "\n=== ADDING ARTS TO USER ==="
+
+  5.times do
+    art_params = {
+      title: Faker::Book.title,
+      location: Faker::Address.full_address,
+      description: Faker::Lorem.paragraph,
+      user: user,
+      price: rand(9.99..1000)
+    }
+
+    art = Art.new(art_params)
+
+    art.category = Category.all.sample
+
+    2.times do
+      photo = URI.open("https://source.unsplash.com/random")
+      art.photos.attach(io: photo, filename: "#{art_params[:title]}#{rand(1..999999)}.png", content_type: "image/png")
+    end
+
+    art.save!
+
+    puts "#{art_params[:title]} added to the arts"
+  end
+end
