1c8e48f5556e8a9d1f2c9032c085abde78755ae0
rutger schoone <rcschoone@gmail.com>
2023-02-18 15:00:12 +0100

    art validations fix

--- app/models/art.rb
+++ app/models/art.rb
@@ -3,6 +3,6 @@ class Art < ApplicationRecord
   has_many_attached :photos
 
   validates :title, :location, :description, :user, :photos, presence: true
-  validates :price, presence: true, greater_than: 0
-  validates :height, :width, numericality: { only_integer: true }, greater_than: 0
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :height, :width, numericality: { only_integer: true, greater_than: 0 }
 end
