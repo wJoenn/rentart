33f3b754bedde1ec9c7531708b464d5d0b41182a
AntonioDannemann <112712290+AntonioDannemann@users.noreply.github.com>
2023-02-21 21:20:28 +0000

    booking model associations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,5 @@
+require "date"
+
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :art
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :arts, dependent: :destroy
+  has_many :bookings, dependent: :destroy
   has_one_attached :photo
 
   validates :first_name, :last_name, :birthdate, presence: true
