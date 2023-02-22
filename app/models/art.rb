61e45a88a14b1ed1f2e50216bc8974ef8624de55
AntonioDannemann <112712290+AntonioDannemann@users.noreply.github.com>
2023-02-22 08:19:03 +0000

    Bookingmodel (#61)
    * booking model generated
    
    * booking model associations
    
    * booking model validations
    
    * removed require date

--- app/models/art.rb
+++ app/models/art.rb
@@ -2,6 +2,7 @@ class Art < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_many_attached :photos
+  has_many :bookings, dependent: :destroy
 
   validates :title, :location, :description, :user, :photos, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
