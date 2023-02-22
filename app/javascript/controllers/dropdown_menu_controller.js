8575d5269710fe5ea2b5d74de3b6e3152493251c
mariana carlota <marianacc99@hotmail.com>
2023-02-22 19:39:52 +0000

    changes to menu toggle

--- app/javascript/controllers/dropdown_menu_controller.js
+++ app/javascript/controllers/dropdown_menu_controller.js
@@ -1,20 +1,17 @@
 import { Controller } from "@hotwired/stimulus";
 import {enter, leave, toggle} from 'el-transition';
 
-console.log("hello from dropdown controller");
-
 export default class extends Controller {
   static targets = ["menu", "button"]
 
   connect() {
-    // call the enter and leave functions
   }
 
   toggleMenu() {
     if(this.menuTarget.classList.contains('hidden')) {
-        enter(this.menuTarget)
+      enter(this.menuTarget)
     } else {
-        leave(this.menuTarget)
+      leave(this.menuTarget)
     }
-}
+  }
 }
