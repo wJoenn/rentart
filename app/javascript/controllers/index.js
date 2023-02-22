f4aada7ce3967d11751882d69abd9ad5c1c43300
rutger schoone <rcschoone@gmail.com>
2023-02-22 17:50:47 +0100

    applied to category show

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,8 @@
 
 import { application } from "./application"
 
+import AosController from "./aos_controller"
+application.register("aos", AosController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
