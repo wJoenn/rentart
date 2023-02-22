dbc38a9abea7d28277c2ddff7465708af573b2f2
AntonioDannemann <112712290+AntonioDannemann@users.noreply.github.com>
2023-02-22 15:31:29 +0000

    revert changes in art controller and modify bookings controller

--- app/controllers/arts_controller.rb
+++ app/controllers/arts_controller.rb
@@ -1,5 +1,5 @@
 class ArtsController < ApplicationController
-  before_action :authenticate_user!, only: %i[index show]
+  skip_before_action :authenticate_user!, only: %i[index show]
 
   def index
     @arts = Art.all
--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,11 +1,10 @@
 class BookingsController < ApplicationController
   def create
-    @booking = Booking.art(bookings_params)
+    @booking = Booking.new(bookings_params)
     @booking.user = current_user
+    @booking.art = Art.find(params[:art_id])
     if Booking.save
       redirect_to art_path(@booking.art)
-    else
-      render 'new', status: :unprocessable_entity
     end
   end
 
