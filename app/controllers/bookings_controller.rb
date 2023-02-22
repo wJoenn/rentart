126abdff2d2d41330b5d2b1ba811df589dd91194
AntonioDannemann <112712290+AntonioDannemann@users.noreply.github.com>
2023-02-22 15:36:12 +0000

    convention style change

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,16 +1,15 @@
 class BookingsController < ApplicationController
   def create
-    @booking = Booking.new(bookings_params)
+    @booking = Booking.new(booking_params)
     @booking.user = current_user
     @booking.art = Art.find(params[:art_id])
-    if Booking.save
-      redirect_to art_path(@booking.art)
-    end
+    Booking.save
+    redirect_to art_path(@booking.art)
   end
 
   private
 
-  def bookings_params
+  def booking_params
     params.require(:booking).permit(:start_date, :end_date, :address)
   end
 end
