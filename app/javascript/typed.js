53197391a869f423c7ce35d7cd2e69f1ad570c69
AntonioDannemann <112712290+AntonioDannemann@users.noreply.github.com>
2023-02-22 17:01:09 +0000

    created typed animation in the main page

---
+++ app/javascript/typed.js
@@ -0,0 +1,12 @@
+import Typed from 'typed.js';
+
+document.addEventListener("DOMContentLoaded", function() {
+  var options = {
+    strings: ["Rentar."],
+    typeSpeed: 400,
+    backSpeed: 200,
+    loop: true
+  }
+
+  var typed = new Typed('.element', options);
+});
