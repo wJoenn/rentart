b91480e3cc8c54f72b182ebe77c22d0d16e4bddf
mariana carlota <marianacc99@hotmail.com>
2023-02-23 23:16:46 +0000

    added method for the bookings and migration for the user

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,9 +1,13 @@
 class Review < ApplicationRecord
-  belongs_to :art, dependent: :destroy
+  belongs_to :art
   belongs_to :user
-  belongs_to :booking
 
-  validates :title, presence: true
-  validates :comment, presence: true, length: { minimum: 5 }
-  validates :rating, presence: true, inclusion: { in: 1..5 },  numericality: { only_integer: true }
+  validates :rating, presence: true, inclusion: 1..5, numericality: { only_integer: true }
+  validate :user_has_booked_before
+
+  private
+
+  def user_had_booked_before
+    user.bookings.map(&:art).include(art)
+  end
 end
