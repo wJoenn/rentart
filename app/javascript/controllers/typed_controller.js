a7f979b8d6d48a20d3186fca9c6bfbd29653fd41
AntonioDannemann <112712290+AntonioDannemann@users.noreply.github.com>
2023-02-23 18:26:49 +0000

    change javascript style

--- app/javascript/controllers/typed_controller.js
+++ app/javascript/controllers/typed_controller.js
@@ -4,14 +4,14 @@ import Typed from "typed.js";
 // Connects to data-controller="typed"
 export default class extends Controller {
   connect() {
-    console.log("hello")
-    var options = {
+
+    const options = {
       strings: ["Rent art.", "Rentart."],
       typeSpeed: 500,
       backSpeed: 200,
       loop: false
     }
 
-    var typed = new Typed(this.element, options);
+    const typed = new Typed(this.element, options);
   }
 }
