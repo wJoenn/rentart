fc4801780da7f970b066f92afc796e62c2d4e8d2
mariana carlota <marianacc99@hotmail.com>
2023-02-23 19:38:41 +0000

    final dropdown

--- app/javascript/controllers/dropdown_menu_controller.js
+++ app/javascript/controllers/dropdown_menu_controller.js
@@ -1,18 +1,24 @@
 import { Controller } from "@hotwired/stimulus";
 
+console.log("Hello again")
+
 export default class extends Controller {
   static targets = ["menu", "button", "options"]
 
   connect() {
-
+    console.log("hello from clicking anywhere")
     window.addEventListener("click", event => {
-      if ((event.target != this.buttonTarget) || (event.target != this.optionsTarget )) {
-        this.menuTarget.classList.add('hidden');
+      if ((event.target != this.buttonTarget) && (event.target != this.optionsTarget)) {
+        this.menuTarget.classList.add('hidden')
       }
+    })
+    window.addEventListener("keyup", event => {
+      if (event.key === "Escape") this.menuTarget.classList.add('hidden')
     })
   }
 
   toggleMenu() {
     this.menuTarget.classList.toggle('hidden')
+    console.log("hi")
   }
 }
