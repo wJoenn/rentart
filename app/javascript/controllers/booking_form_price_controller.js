f776e8bce6cbcd51cf9df60dff814a3dc9e5bd7d
Louis Ramos <louisramosdev@gmail.com>
2023-02-25 11:20:28 +0100

    Fixed a bug when changing start date after inputing end date

--- app/javascript/controllers/booking_form_price_controller.js
+++ app/javascript/controllers/booking_form_price_controller.js
@@ -2,24 +2,18 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="booking-form-price"
 export default class extends Controller {
-  connect() {
-    console.log("h");
-  }
-
   static targets = ["startDate", "endDate", "price", "field", "days"]
 
   enabledEndDateField() {
+    this.endDateTarget.value = ""
     this.endDateTarget.min = new Date(new Date(this.startDateTarget.value).valueOf() + 86400000).toISOString().split('T')[0]
-    console.log(new Date(new Date(this.startDateTarget.value).valueOf() + 86400000).toISOString().split('T')[0]);
     this.endDateTarget.disabled = false
-    console.log(this.endDateTarget);
   }
 
   updatePrice() {
     const days = Math.floor(((new Date(this.endDateTarget.value)) - (new Date(this.startDateTarget.value))) / 86400000)
     const price = (+this.priceTarget.value).toFixed(2)
 
-    console.log(days);
     this.daysTarget.innerText = ""
     this.fieldTarget.innerText = ""
 
