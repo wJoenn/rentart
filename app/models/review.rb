27a1fbcd4e1fd71edc7f0a0a0bf51959d502fc61
MarianaCarlota <114437310+MarianaCarlota@users.noreply.github.com>
2023-02-25 13:59:54 +0000

    Update review.rb

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,13 +3,14 @@ class Review < ApplicationRecord
   belongs_to :user
 
   validates :rating, presence: true, inclusion: 1..5, numericality: { only_integer: true }
+  validates :user_id, uniqueness: { scope: :art_id, message: "has already reviewed this listing" }
   validate :user_has_booked_before
-  validates :art_id, uniqueness: { scope: :user_id, message: "You've already reviewed this piece" }
 
   private
 
   def user_has_booked_before
     return if user.bookings.map(&:art).include?(art)
 
-    errors.add(:user, "Sorry, you can't review a piece you have never booked")
+    errors.add(:user, "cannot review a listing he has never booked")
+  end
 end
