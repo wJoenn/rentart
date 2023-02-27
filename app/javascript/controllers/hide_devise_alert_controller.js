afab4c22cff470e805d354bd430f6f183055d049
AntonioDannemann <112712290+AntonioDannemann@users.noreply.github.com>
2023-02-27 15:58:42 +0000

    refactor on javascrypt controller, and changes to scss

--- app/javascript/controllers/hide_devise_alert_controller.js
+++ app/javascript/controllers/hide_devise_alert_controller.js
@@ -2,7 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="hide-devise-alert"
 export default class extends Controller {
-  static targets = ["alert"];
 
   connect() {
     if (this.element) {
