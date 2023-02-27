ade1f5ff482a241fbb35aa02e0681ce85acdc0da
Louis Ramos <75388869+wJoenn@users.noreply.github.com>
2023-02-27 17:41:42 +0100

    Added an animation to the alert (#112)

--- app/javascript/controllers/hide_devise_alert_controller.js
+++ app/javascript/controllers/hide_devise_alert_controller.js
@@ -2,16 +2,24 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="hide-devise-alert"
 export default class extends Controller {
-
   connect() {
     if (this.element) {
+      this.#showAlert()
       setTimeout(() => {
-        this.hideAlert();
+        this.#hideAlert();
       }, 3000);
     }
   }
 
-  hideAlert() {
-    this.element.style.display = "none";
+  #hideAlert() {
+    this.element.style.maxHeight = "0px";
+    this.element.style.padding = "0px"
+    this.element.style.opacity = "0";
+  }
+
+  #showAlert() {
+    this.element.style.maxHeight = "80px";
+    this.element.style.padding = "20px"
+    this.element.style.opacity = "1";
   }
 }
